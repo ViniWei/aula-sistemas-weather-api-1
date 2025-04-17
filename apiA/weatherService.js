381ff29e6e1fd6f8bd0daf5b6f038f9e87dec89b
Vinícius Weigert <vweigert@gocn.com.br>
2025-04-16 21:34:55 -0300

    Added cache to weather

--- apiA/weatherService.js
+++ apiA/weatherService.js
@@ -1,14 +1,28 @@
+import { createClient } from "redis";
+const client = createClient();
+
+client.on('error', err => console.log('Redis Client Error', err));
+await client.connect();
+
+
 const weatherRoute = "http://localhost:4000/weather/";
 
 const getweather = async(city) => {
-   const response = await fetch(weatherRoute + city);
-    
-    if (!response.ok) {
-        throw new Error(`Response status: ${response.status}`);
+    if (!await client.get(city)) {
+        const response = await fetch(weatherRoute + city);
+
+        if (!response.ok) {
+            throw new Error(`Response status: ${response.status}`);
+        }
+
+        const json = await response.json();
+        await client.setEx(city, 10, JSON.stringify(json));
+
+        console.log("sem", json.temp)
+        return json.temp;
     }
 
-    const json = await response.json();
-    return json.temp;
+    return JSON.parse(await client.get(city)).temp
 }
 
 export default {
